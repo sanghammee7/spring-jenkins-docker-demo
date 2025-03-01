790c6534fda67f5d5a1513085188be6a790dd706
sanghammee <sanghammee@lavendelconsulting.com>
2025-03-01 16:27:14 +0100

    Adding Test file to the project

---
+++ src/main/java/com/example/spring/jenkins/docker/spring/jenkins/docker/demo/controller/Test.java
@@ -0,0 +1,7 @@
+package com.example.spring.jenkins.docker.spring.jenkins.docker.demo.controller;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
