e1f1b317c4a43abd2defeabd2d809c8a3da8159e
sanghammee <sanghammee@lavendelconsulting.com>
2025-03-01 16:33:05 +0100

    adding Log info

--- src/main/java/com/example/spring/jenkins/docker/spring/jenkins/docker/demo/SpringJenkinsDockerDemoApplication.java
+++ src/main/java/com/example/spring/jenkins/docker/spring/jenkins/docker/demo/SpringJenkinsDockerDemoApplication.java
@@ -1,12 +1,18 @@
 package com.example.spring.jenkins.docker.spring.jenkins.docker.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsDockerDemoApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsDockerDemoApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Application started ...");
+
 		SpringApplication.run(SpringJenkinsDockerDemoApplication.class, args);
 	}
 
