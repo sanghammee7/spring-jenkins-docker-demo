21f3a96ad5e1a02c969238cd81422b2b4d0a611a
sanghammee <sanghammee@lavendelconsulting.com>
2025-03-01 16:08:33 +0100

    first commit

---
+++ src/main/java/com/example/spring/jenkins/docker/spring/jenkins/docker/demo/SpringJenkinsDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.jenkins.docker.spring.jenkins.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/jenkins/docker/spring/jenkins/docker/demo/SpringJenkinsDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.jenkins.docker.spring.jenkins.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
